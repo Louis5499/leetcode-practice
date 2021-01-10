59578cb92f4837ff0a96e3e9e6d9045d4b9b8ff1
Louis5499 <louisby0123@gmail.com>
2021-01-10 22:34:59 +0800

    Adjust style ;

--- 309/solution.cpp
+++ 309/solution.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
 	int maxProfit(vector<int>& prices){
 		if (prices.size() <= 1) return 0;
-        int s0, s1, s2;
+    int s0, s1, s2;
 		s1 = -prices[0];
 		s0 = 0;
 		s2 = INT_MIN;
 		for (int i = 1; i < prices.size(); i++) {
-            int prev_s0 = s0;
+      int prev_s0 = s0;
 			s0 = max(prev_s0, s2);
-            s2 = s1 + prices[i];
+      s2 = s1 + prices[i];
 			s1 = max(s1, prev_s0 - prices[i]);
 		}
 		return max(s0, s2);
