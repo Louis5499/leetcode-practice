abd442a6b73d95e58279f3a399d84862a8b62fd3
Louis5499 <louisby0123@gmail.com>
2021-02-11 11:51:05 +0800

    add problem 650

---
+++ 650/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> dp(n+1, 0);
+        
+        for (int i=2; i<=n; i++) {
+            for (int j = i/2; j >= 1; j--) {
+                if (i%j == 0) {
+                    dp[i] = dp[j] + (i/j);
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
