4b6fffe6bacf39400097f0d2be39234ce4f22e83
Louis5499 <louisby0123@gmail.com>
2021-05-12 11:50:16 +0800

    add problem 70

---
+++ 70/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> steps(n+1, 0);
+        steps[0] = 0;
+        steps[1] = 1;
+        if (n < 2) return steps[n];
+        steps[2] = 2;
+        for (int i=3; i<=n;i++) {
+            steps[i] = (steps[i-2] + steps[i-1]);
+        }
+        return steps[n];
+    }
+};
