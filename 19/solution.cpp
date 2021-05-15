ca57dbe03533603ba842e66b15ab38cc6bf6eb98
Louis5499 <louisby0123@gmail.com>
2021-05-15 15:31:12 +0800

    add problem 19

---
+++ 19/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode *slow = dummy, *fast = dummy;
+        
+        for (int i=0; i<=n; i++) fast = fast->next;
+        
+        while(fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
+};
