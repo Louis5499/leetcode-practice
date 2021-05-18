7c5f403ea67df26eec14715a844e16f3f3dc5b2e
Louis5499 <louisby0123@gmail.com>
2021-05-18 10:34:56 +0800

    add problem 20

---
+++ 20/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char c: s) {
+            if (c == '(') stk.push(')');
+            else if (c == '[') stk.push(']');
+            else if (c == '{') stk.push('}');
+            else if (stk.empty() || stk.top() != c) {
+                return false;
+            }
+            else stk.pop();
+        }
+        return stk.empty();
+    }
+};
