fdebd336aa7e6b02bb4af441222608be2f58e462
Louis5499 <louisby0123@gmail.com>
2021-05-21 15:15:24 +0800

    modify problem 347

--- 347/solution.cpp
+++ 347/solution.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> count;
-        priority_queue<int, vector<int>, greater<int>> maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
         
         for (int num: nums) count[num]++;
         
         for (auto record: count) {
-            maxHeap.push(record.second);
+            minHeap.push(record.second);
             
-            if (maxHeap.size() > k) maxHeap.pop();
+            if (minHeap.size() > k) minHeap.pop();
         }
         
         vector<int> res;
         for (auto record: count) {
-            if (record.second >= maxHeap.top()) res.emplace_back(record.first);
+            if (record.second >= minHeap.top()) res.emplace_back(record.first);
         }
         return res;
     }
