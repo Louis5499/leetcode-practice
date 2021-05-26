7ff30e6862a0c88da3e8bd35fff663a7f4b4ae70
Louis5499 <louisby0123@gmail.com>
2021-05-26 11:14:02 +0800

    add problem 277

---
+++ 277/solution.cpp
@@ -0,0 +1,20 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int candidate = 0;
+        for (int i=0; i<n; i++) {
+            if (knows(candidate, i)) {
+                candidate = i;
+            }
+        }
+        
+        for (int i=0; i<n; i++) {
+            if (i < candidate && (knows(candidate, i) || !knows(i, candidate))) return -1;
+            if (i > candidate && !knows(i, candidate)) return -1;
+        }
+        return candidate;
+    }
+};
