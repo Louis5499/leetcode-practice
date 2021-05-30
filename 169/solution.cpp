f3beae0186d275870d31b1a1874e9a9196f6fcdc
Louis5499 <louisby0123@gmail.com>
2021-05-30 12:16:13 +0800

    add problem 169

---
+++ 169/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+        
+        for (int num: nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (candidate == num ? 1 : -1);
+        }
+        return candidate;
+    }
+};
