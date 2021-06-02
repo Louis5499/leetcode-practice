97630c4d612406888ba4b4685dd63c73c3d9fe3f
Louis5499 <louisby0123@gmail.com>
2021-06-02 10:43:27 +0800

    add problem 240

---
+++ 240/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size()-1, col = 0;
+        
+        while (row >= 0 && col < matrix[0].size()) {
+            if (matrix[row][col] > target) row--;
+            else if (matrix[row][col] < target) col++;
+            else return true;
+        }
+        return false;
+    }
+};
