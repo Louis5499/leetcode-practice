7732cc27dd52cd490e766a247518c778c617b470
Louis5499 <louisby0123@gmail.com>
2021-06-25 16:06:57 +0800

    add problem 117

--- 117/solution.cpp
+++ 117/solution.cpp
@@ -19,19 +19,26 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if (root == NULL) return NULL;
-        queue<Node *> q;
-        q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            Node *prevNode = NULL;
-            while (n--) {
-                Node *node = q.front(); q.pop();
-                if (prevNode != NULL) prevNode->next = node;
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-                prevNode = node;
+        Node *prev = NULL, *head = NULL, *cur = root;
+        
+        while (cur != NULL) {
+            while (cur != NULL) {
+                if (cur->left) {
+                    if (prev != NULL) prev->next = cur->left;
+                    else head = cur->left;
+                    prev = cur->left;
+                }
+                if (cur->right) {
+                    if (prev != NULL) prev->next = cur->right;
+                    else head = cur->right;
+                    prev = cur->right;
+                }
+                cur = cur->next;
             }
+            
+            cur = head;
+            prev = NULL;
+            head = NULL;
         }
         
         return root;
