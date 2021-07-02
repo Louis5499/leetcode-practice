e879dbf4fe1476830778b5aaaa54daedf260a985
Louis5499 <louisby0123@gmail.com>
2021-07-02 10:32:02 +0800

    add problem 518

---
+++ 518/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1, 0);
+        dp[0] = 1;
+        for (int i=1; i<=coins.size(); i++) {
+            for (int j=1; j<=amount; j++) {
+                dp[j] = dp[j] + (j >= coins[i-1] ? dp[j-coins[i-1]] : 0);
+            }
+        }
+        return dp[amount];
+        
+        
+        // vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 0));
+        // dp[0][0] = 1;
+
+        // for (int i=1; i<=coins.size(); i++) {
+        //     dp[i][0] = 1;
+        //     for (int j=1; j<=amount; j++) {
+        //         dp[i][j] = dp[i-1][j] + ( j >= coins[i-1] ? dp[i][j-coins[i-1]] : 0 );
+        //     }
+        // }
+        
+        // return dp[coins.size()][amount];
+    }
+};
