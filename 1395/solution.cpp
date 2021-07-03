9b95c5f8c87090dc6ef3241b9a24af8c8a780c9d
Louis5499 <louisby0123@gmail.com>
2021-07-03 11:15:42 +0800

    renew problem 1395

--- 1395/solution.cpp
+++ 1395/solution.cpp
@@ -13,3 +13,34 @@ public:
         return res;
     }
 };
+
+/* Another solution
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n = rating.size(), res = 0;
+        // Step1. Use two set to store ratings that we've seen, e.g.: [1,2,3,4]
+        // iterate:  1    /    2      /   3      /    4
+        // lset:          /    1      /  1,2     / 1,2,3
+        // rset: 1,2,3,4  /  2,3,4    /  3,4     /    4
+        set<int> lset, rset(rating.cbegin(), rating.cend());
+        for(int i=1; i<n ;i++){
+            lset.insert(rating[i-1]); //Put prev value into the lset
+            rset.erase(rating[i-1]);  //Erase prev value from the rset
+             
+            // Step 2. for rating[i] < rating[j] < rating[k]
+            int d1 = std::distance(lset.cbegin(), lset.lower_bound(rating[i])); //Check how many values are smaller than rating[i]
+            int d2 = std::distance(rset.upper_bound(rating[i]), rset.cend());   //Check how many values are bigger than rating[i]
+            if(d1 > 0 && d2 > 0)
+                res += d1*d2;
+            
+            // Step 3. for rating[i] > rating[j] > rating[k]
+            int d3 = std::distance(lset.upper_bound(rating[i]), lset.cend()); //Check how many values are bigger than rating[i]
+            int d4 = std::distance(rset.cbegin(), rset.lower_bound(rating[i])); //Check how many values are smaller than rating[i]
+            if(d3 > 0 && d4 > 0)
+                res += d3*d4;     
+        }
+        return res;
+    }
+};
+*/
