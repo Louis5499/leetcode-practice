ecfc9e766c53ca5e719b522ea5dc0729d3f0da66
Louis5499 <louisby0123@gmail.com>
2021-07-08 10:46:50 +0800

    add problem 532

---
+++ 532/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if (k < 0) {
+            return 0;
+        }
+        unordered_set<int> starters;
+        unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.count(nums[i] - k)) {
+                starters.insert(nums[i] - k);
+            }
+            if (indices.count(nums[i] + k)) {
+                starters.insert(nums[i]);
+            }
+
+            indices[nums[i]]++;
+        }
+        
+        return starters.size();
+    }
+};
